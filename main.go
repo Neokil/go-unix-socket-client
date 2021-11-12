89e53dad7c77ae59157876c8c4d242d8ef5c13c1
Simon Schulte <simon.schulte@nfon.com>
2021-11-12 15:29:17 +0100

    Initial Commit

--- main.go
+++ main.go
@@ -11,12 +11,42 @@ import (
 
 func main() {
 	s := flag.String("socket", "", "Specifies the socket to connect to")
+	c := flag.String("command", "", "Specifies a command to run. If not set interactive mode will be used.")
 	flag.Parse()
 
 	if s == nil || *s == "" {
 		panic("-socket parameter is required")
 	}
 
+	if c != nil && *c != "" {
+		executeCommand(*c, *s)
+		return
+	}
+
+	interactiveMode(*s)
+}
+
+func executeCommand(cmd string, socket string) {
+	c, err := net.Dial("unix", socket)
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+
+	_, err = c.Write([]byte(cmd + "\n"))
+	if err != nil {
+		panic(err)
+	}
+
+	r := bufio.NewReader(c)
+	line, err := r.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(line)
+}
+
+func interactiveMode(socket string) {
 	r := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Print("<-")
@@ -25,10 +55,11 @@ func main() {
 			panic(err)
 		}
 
-		c, err := net.Dial("unix", *s)
+		c, err := net.Dial("unix", socket)
 		if err != nil {
 			panic(err)
 		}
+		defer c.Close()
 		_, _ = c.Write([]byte(line))
 
 		b, err := ioutil.ReadAll(c)
